1b37767813dea80ad6b9b0fa3697441eb55db72f
jinjutha-17 <jinjutha.tho@spumail.net>
2026-03-12 15:40:33 +0700

    Update views.py
    update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 def form_view(request):
+    success = False
+
     if request.method == "POST":
-        return render(request, 'form.html', {'success': True})
-    
-    return render(request, 'form.html')
+        fullname = request.POST.get("fullname")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+
+        if fullname and email and phone:
+            success = True
+
+    return render(request, 'form.html', {'success': success})
